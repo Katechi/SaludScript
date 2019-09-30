2dfc259cdd56694456ea8fca5d5e1e1da9859a5e
Nicolás <Ncollio2016@alu.uct.cl>
2019-09-30 20:00:24 -0300

    avance

--- Diseño Pagina/SaludScript/app.js
+++ Diseño Pagina/SaludScript/app.js
@@ -8,7 +8,7 @@ app.set('view engine','ejs');
 
 //home route
 app.get('/', (req,res) => {
-    res.render('index');
+    res.render('home');
 });
 app.listen(3000, () => {
     console.log('está en el port 3000');
