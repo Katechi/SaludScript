b29787629d4acf65cd17e5d20a6efdc7b05da9de
Nicolás <Ncollio2016@alu.uct.cl>
2019-09-30 21:17:43 -0300

    modificacion

--- Diseño Pagina/SaludScript/app.js
+++ Diseño Pagina/SaludScript/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const authRoutes = require('./routes/ruta_autori');
 const app = express();
 
 //set up
 
 app.set('view engine','ejs');
+
+//set up routes
+app.use('/auth', authRoutes);
 
 //home route
 app.get('/', (req,res) => {
--- Diseño Pagina/SaludScript/routes/ruta-autori.js
+++ Diseño Pagina/SaludScript/routes/ruta-autori.js
@@ -4,3 +4,16 @@ const router = require('express').Router();
 router.get('/login',(req,res) => {
     res.render('login');
 });
+
+//auth logout
+router.get('/logout',(req,res) => {
+    res.send('logging out');
+});
+
+//auth con google
+
+router.get('/google',(req,res) => {
+    res.send('logging in con google');
+});
+
+module.exports = router;
